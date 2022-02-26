b8f5a2a2421f716ca677334291ad152263d12ea9
tane-git <tane.alexander.wilson@gmail.com>
2022-02-26 22:37:16 +1300

    gacp

--- aliases.zsh
+++ aliases.zsh
@@ -5,6 +5,11 @@ add-this () {
   source ~/.zshrc
 }
 
+gacp () {
+  gaa && gcmsg '$1' && ggp
+  # echo "gaa && gcmsg '$1' && ggp"
+}
+
 alias aliases='code /home/tane_wilson/.oh-my-zsh/custom/aliases.zsh'
 alias yi='yarn install'
 alias yb='yarn build'
@@ -12,3 +17,8 @@ alias ya='yarn add'
 alias ycg='yarn codegen'
 alias dock='docker-compose pull && docker-compose up'
 alias destroy='sudo rm -rf'
+alias test='echo test'
+alias sq-explrr='cd ~/Code/subQuery/subquery-explorer'
+alias sq-website='cd ~/Code/subQuery/subquery-website'
+alias ys='yarn start'
+alias gac='gaa && gcmsg'
