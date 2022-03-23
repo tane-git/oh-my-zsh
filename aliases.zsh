988259711642b596eaba04044cfc7fbe6fa59a9c
tane-git <tane.alexander.wilson@gmail.com>
2022-03-24 09:34:01 +1300

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,4 @@
-add-this () {
+add () {
   # add-this <alias> <command>
   # echo "alias "$1"='"$2"'" >> /home/tane_wilson/.oh-my-zsh/custom/aliases.zsh
   echo "alias "$1"='"$2"'" >> /Users/tane/.oh-my-zsh/custom/aliases.zsh
@@ -6,7 +6,7 @@ add-this () {
 }
 
 gacp () {
-  gaa && gcmsg '$1' && ggp
+  gaa && gcmsg "$1" && ggp
   # echo "gaa && gcmsg '$1' && ggp"
 }
 
@@ -18,7 +18,11 @@ alias ycg='yarn codegen'
 alias dock='docker-compose pull && docker-compose up'
 alias destroy='sudo rm -rf'
 alias test='echo test'
-alias sq-explrr='cd ~/Code/subQuery/subquery-explorer'
+alias sq-explorer='cd ~/Code/subQuery/subquery-explorer'
 alias sq-website='cd ~/Code/subQuery/subquery-website'
+alias sq-console='cd ~/Code/subQuery/subquery-console'
 alias ys='yarn start'
 alias gac='gaa && gcmsg'
+alias ysb='yarn storybook'
+alias website='cd ~/code/subquery/projects/subquery-website'
+alias ybys='yarn build && yarn start'
