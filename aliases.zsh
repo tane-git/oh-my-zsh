503a342118556cfb910f31f053d1a1d2532640f5
tane-git <tane.alexander.wilson@gmail.com>
2022-06-14 13:58:20 +1200

    update

--- aliases.zsh
+++ aliases.zsh
@@ -10,19 +10,35 @@ gacp () {
   # echo "gaa && gcmsg '$1' && ggp"
 }
 
-alias aliases='code /home/tane_wilson/.oh-my-zsh/custom/aliases.zsh'
+alias aliases='code /Users/tane/.oh-my-zsh/custom'
+
+# Yarn:
 alias yi='yarn install'
 alias yb='yarn build'
 alias ya='yarn add'
 alias ycg='yarn codegen'
+alias ys='yarn start'
+alias ysb='yarn storybook'
+alias ybys='yarn build && yarn start'
+
+# Docker:
 alias dock='docker-compose pull && docker-compose up'
+
+# General:
 alias destroy='sudo rm -rf'
 alias test='echo test'
-alias sq-explorer='cd ~/Code/subQuery/subquery-explorer'
-alias sq-website='cd ~/Code/subQuery/subquery-website'
-alias sq-console='cd ~/Code/subQuery/subquery-console'
-alias ys='yarn start'
+alias python='python3'
+alias patrick='cd ~/Code/tutorials/WEB3/FFC/PatrickLearnWeb3-JS/full-blockchain-solidity-course-js'
+
+# Git:
 alias gac='gaa && gcmsg'
-alias ysb='yarn storybook'
+
+# Subquery Projects:
+alias subquery='cd code/subquery'
+alias explorer='cd ~/code/subquery/projects/subquery-explorer'
+alias console='cd code/subquery/projects/subquery-console'
 alias website='cd ~/code/subquery/projects/subquery-website'
-alias ybys='yarn build && yarn start'
+alias ui-library='cd ~/Code/subQuery/Projects/LIBRARIES/subquery-ui'
+alias libraries='cd ~/code/subquery/projects/libraries'
+
+# New:
